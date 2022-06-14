b89be311205422b3b08d324470e3157ffd968ff4
Kestin Goforth <kgoforth1503@gmail.com>
2022-06-14 11:18:14 -0400

    Copy firmware.hex to root dir on build

---
+++ extra_script.py
@@ -0,0 +1,13 @@
+import os
+
+Import("env", "projenv")
+
+# Copy firmware.hex to root dir
+env.AddPostAction(
+    "$BUILD_DIR/${PROGNAME}.hex",
+    env.VerboseAction(" ".join([
+        "$OBJCOPY", 
+        f'"{os.path.join("$BUILD_DIR", "${PROGNAME}.hex")}"',
+        f'"{os.path.join("$PROJECT_DIR", "f430-1.1.6-mod.hex")}"',
+    ]), "Copying ${PROGNAME}.hex")
+)
