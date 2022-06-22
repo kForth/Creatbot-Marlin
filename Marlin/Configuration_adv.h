4bdf01fbd054057270e422a5f4cdb928cb911a0f
Kestin Goforth <kgoforth1503@gmail.com>
2022-06-22 10:43:17 -0400

    Enable workspace offsets

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1261,7 +1261,7 @@
  *  - M206 and M428 are disabled.
  *  - G92 will revert to its behavior from Marlin 1.0.
  */
-#define NO_WORKSPACE_OFFSETS			//(By LYN, default is comment)
+//#define NO_WORKSPACE_OFFSETS
 
 /**
  * Set the number of proportional font spaces required to fill up a typical character space.
