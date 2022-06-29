fb1adc1bd087435b4a9374bfb2d726e56e427243
Kestin Goforth <kgoforth1503@gmail.com>
2022-06-29 14:53:48 -0400

    Enable FILAMENT_RUNOUT_DETECTION

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -14852,7 +14852,16 @@ void customDetect() {
 void manage_inactivity(bool ignore_stepper_queue/*=false*/) {
 
   #if ENABLED(FILAMENT_RUNOUT_SENSOR)
-    if ((IS_SD_PRINTING || print_job_timer.isRunning()) && (READ(FIL_RUNOUT_PIN) == FIL_RUNOUT_INVERTING))
+    if ((!FILE_IS_PRINT && print_job_timer.isRunning()) && (
+      (READ(FIL_RUNOUT_PIN) == FIL_RUNOUT_INVERTING)
+      //TODO: Check pin for active extruder
+      // #if PIN_EXISTS(FIL_RUNOUT1)
+      //   || (READ(FIL_RUNOUT1_PIN) == FIL_RUNOUT_INVERTING)
+      // #endif
+      // #if PIN_EXISTS(FIL_RUNOUT2)
+      //   || (READ(FIL_RUNOUT2_PIN) == FIL_RUNOUT_INVERTING)
+      // #endif
+    ))
       handle_filament_runout();
   #endif
 
