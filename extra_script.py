c8751e7ac0bd9b7a8b9690002239acdc565f87d8
Kestin Goforth <kgoforth1503@gmail.com>
2022-07-11 11:47:53 -0400

    Update extra_script.py

--- extra_script.py
+++ extra_script.py
@@ -1,14 +1,24 @@
 import os
 from datetime import datetime
 
+OUT_FILE = "f430-1.1.6.hex"
+
 Import("env", "projenv")
 
 # Copy firmware.hex to root dir
 env.AddPostAction(
-    "$BUILD_DIR/${PROGNAME}.hex",
+    "$BUILD_DIR/${PROGNAME}.elf",
     env.VerboseAction(" ".join([
-        "$OBJCOPY", 
-        f'"{os.path.join("$BUILD_DIR", "${PROGNAME}.hex")}"',
-        f'"{os.path.join("$PROJECT_DIR", "f430-1.1.6.hex")}"',
-    ]), f"[{datetime.now().strftime('%H:%M:%S')}] Copying ${{PROGNAME}}.hex to f430-1.1.6.hex")
+        "$OBJCOPY", "-O", "ihex", "-R", ".eeprom",
+        f'"{os.path.join("$BUILD_DIR", "${PROGNAME}.elf")}"',
+        f'"{os.path.join("$PROJECT_DIR", OUT_FILE)}"',
+    ]), f"Building {OUT_FILE}")
+)
+env.AddPostAction(
+    "$BUILD_DIR/${PROGNAME}.elf",
+    env.VerboseAction(" ".join([
+        "$OBJCOPY", "-O", "ihex", "-R", ".eeprom", 
+        '"%s"' % os.path.join("$BUILD_DIR", "${PROGNAME}.elf"),
+        '"%s"' % os.path.join("\\\\LIGHTPIPE\\F430 Firmware Files", OUT_FILE),
+    ]), f"Building {OUT_FILE} on LIGHTPIPE PC")
 )
